9a4de4fb6597d4f0fe6846b684626822af27ec0f
Brayan Frank <brayanfrank.sm@gmail.com>
2022-04-20 15:48:04 -0300

    finishing section 02

--- src/components/mega/Mega.jsx
+++ src/components/mega/Mega.jsx
@@ -1,48 +1,53 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+
+import './Mega.css';
 
 export default props => {
 
-    const [numValues, setNumValues] = useState()
+    const [numValues, setNumValues] = useState(5)
     const [generatedNumbers, setgeneratedNumbers] = useState([])
 
     return (
         <div>
             <label>Digite o número a ser gerado (5 à 15):</label>
-            <input type='number' min={5} max={15} onChange={(e) => setNumValues(+e.target.value)}></input>
-            <button onClick={() => setgeneratedNumbers(randomValues(1,60, numValues))}>
-                Gerar
-            </button>
-            <div>
-            {
-               generatedNumbers == -1 ? 
-                    <p>Algum problema ocorreu!</p>
-                :
-                    generatedNumbers.map(number => {
-                        if(generatedNumbers[generatedNumbers.length - 1] == number)
-                            return <span>{number}</span>
-                        return <span>{number}, </span>
-                    })
+            <input type='number' min={5} max={15} value={numValues} onChange={
+                (e) => {
+                    setNumValues(+e.target.value);
+                    setgeneratedNumbers(randomValues(+e.target.value))
+                }
             }
+            ></input>
+
+            <div className='mega-result-div'>
+                {
+                    generatedNumbers == -1 ?
+                        <p>Algum problema ocorreu!</p>
+                        :
+                        generatedNumbers.map(number => {
+                            return <div className='mega-result'>{number} </div>
+                        })
+                }
             </div>
         </div>
     )
 }
 
-function randomValues(min,max, numValues) {
+function randomValues( numValues) {
 
     let arrRandomValues = []
+    let min = 1 
+    let max = 60
 
-    if(!(numValues <  5 || numValues > 15)) {
-        for(let i = 0; i < numValues;i++) {
+    if (!(numValues < 5 || numValues > 15)) {
+        for (let i = 0; i < numValues; i++) {
             let randomNum = Math.round(Math.random() * (max - min) + min)
-            if(!arrRandomValues.includes(randomNum))
-            {
-                arrRandomValues.push(randomNum)   
+            if (!arrRandomValues.includes(randomNum)) {
+                arrRandomValues.push(randomNum)
             }
             else i--
         }
 
-        arrRandomValues.sort( (a, b) => {
+        arrRandomValues.sort((a, b) => {
             return a - b;
         });
 
@@ -50,5 +55,5 @@ function randomValues(min,max, numValues) {
     }
 
     return -1
-} 
+}
 
